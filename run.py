2b746d112a599e02864c9882cf910a485121c7ac
Bhavesh Gawri <localghost@jbook.com>
2023-04-25 01:22:57 -0400

    save model fn

--- run.py
+++ run.py
@@ -1,35 +1,26 @@
-from classes import CheetahTransformer, CheetahDataLoader
-from transformers import DecisionTransformerConfig
-from torch.optim import AdamW
-from torch.nn.utils.clip_grad import clip_grad_norm_
-from torch.optim.lr_scheduler import LambdaLR
+from run_utils import train, save_model
+
+import time
+
+runtime_env='dev'
 
 state_dimension, action_dimension = 17, 6
 max_ep_len, train_ep_len = 1000, 20
 epochs, itr, batch_size = 40, 45, 64
 warmup_steps, grad_clip = 10000, 0.25
-drop_out = 0.1
+drop_out, gamma = 0.1, 0.99
 lr, weight_decay = 0.0001, 0.0001
 
-cdl = CheetahDataLoader(max_ep_len=max_ep_len, train_ep_len=train_ep_len, gamma=0.99)
-cfg = DecisionTransformerConfig(state_dim=state_dimension, act_dim=action_dimension, max_ep_len=max_ep_len, drop_out=drop_out)
-model = CheetahTransformer(cfg)
-optim = AdamW(model.parameters(), lr=lr, weight_decay=weight_decay)
-sched = LambdaLR(optim, lambda steps: min((steps + 1) / warmup_steps, 1))
+if runtime_env == 'dev':
+    epochs = 5
+    itr = 1
 
-for epoch in range(epochs):
-    for i in range(itr):        
-        x = cdl.get_batch(batch_size=batch_size)
-        y_pred = model(**x)
+curr_time = int(time.time())
+model = train(max_ep_len, train_ep_len, gamma, state_dimension, action_dimension, 
+                drop_out, lr, weight_decay, warmup_steps, epochs, itr, batch_size, 
+                grad_clip)
 
-        action_inp, action_out, mask = x['actions'], y_pred[1], x['attention_mask']
-        loss = model.calc_loss(action_inp, action_out, mask)
+save_model(model, env='cheetah', type_='sc', time=curr_time)
 
-        optim.zero_grad()
-        loss.backward()
 
-        clip_grad_norm_(model.parameters(), grad_clip)
-        optim.step()
-        sched.step()
 
-    print(f'On epoch: {epoch + 1}, loss: {loss}')
